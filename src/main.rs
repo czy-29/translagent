a4fa643aa0296af9b15dcb21dff0b0756aa7bf32
29 <791603901@qq.com>
2026-04-06 20:42:01 +0800

    init_tracing_with_ansi

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
+use tracing_subscriber::fmt::{format::FmtSpan, time::ChronoLocal};
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    init_tracing_with_ansi();
     println!("Hello, world!");
     Ok(())
+}
+
+// 参见：https://github.com/tokio-rs/tracing/issues/3068
+fn init_tracing_with_ansi() {
+    #[cfg(windows)]
+    nu_ansi_term::enable_ansi_support().ok();
+    init_tracing();
+}
+
+#[cfg(debug_assertions)]
+fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
+        .with_max_level(tracing::Level::DEBUG)
+        .with_span_events(FmtSpan::FULL)
+        .with_thread_names(true)
+        .init();
+}
+
+#[cfg(not(debug_assertions))]
+fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
+        .with_span_events(FmtSpan::FULL)
+        .with_thread_names(true)
+        .init();
 }
